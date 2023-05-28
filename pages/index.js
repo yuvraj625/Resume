1737f81fca036897c447818767048e176642198e
Yuvraj_Singh310 <109100137+yuvraj625@users.noreply.github.com>
2023-05-28 08:37:03 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -128,7 +128,7 @@ export default function Home() {
                 Relevent Skills
               </h3>
               <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
+                Do you have an idea for your next great website? Lets make it a
                 reality.
               </p>
               <h4 className="py-4 text-teal-600">Tools I Use</h4>
@@ -168,7 +168,7 @@ export default function Home() {
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
-              developer, I've added valuable content for
+              developer, I have added valuable content for
               <span className="text-teal-500"> Local Businesses </span>
               and for <span className="text-teal-500">startups </span>
               and collaborated with talanted people to create digital products
