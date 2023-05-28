535c98b578c52f18202c8ef63e5127fc3cec55d6
Yuvraj_Singh310 <109100137+yuvraj625@users.noreply.github.com>
2023-05-28 08:30:34 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,12 @@ export default function Home() {
               <AiFillYoutube />
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" />
+              <Image
+                src={deved}
+                alt="avatar picture"
+                layout="fill"
+                objectFit="cover"
+              />
             </div>
           </div>
         </section>
@@ -94,7 +99,12 @@ export default function Home() {
           </div>
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} />
+              <Image
+                src={design}
+                alt="avatar picture"
+                width={100}
+                height={100}
+              />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
                 Education Background
               </h3>
@@ -113,7 +123,7 @@ export default function Home() {
               <p className="text-gray-800 py-1"></p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} />
+              <Image src={code} alt="avatar picture" width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 Relevent Skills
               </h3>
@@ -130,7 +140,12 @@ export default function Home() {
               <p className="text-gray-800 py-1">Mongo DB</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
+              <Image
+                src={consulting}
+                alt="avatar picture"
+                width={100}
+                height={100}
+              />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 EXTRA CURRICULAR
               </h3>
@@ -172,6 +187,7 @@ export default function Home() {
                 height={"100%"}
                 layout="responsive"
                 src={web1}
+                alt="avatar picture"
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -181,6 +197,7 @@ export default function Home() {
                 height={"100%"}
                 layout="responsive"
                 src={web2}
+                alt="avatar picture"
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -190,6 +207,7 @@ export default function Home() {
                 height={"100%"}
                 layout="responsive"
                 src={web3}
+                alt="avatar picture"
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -199,6 +217,7 @@ export default function Home() {
                 height={"100%"}
                 layout="responsive"
                 src={web4}
+                alt="avatar picture"
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -208,6 +227,7 @@ export default function Home() {
                 height={"100%"}
                 layout="responsive"
                 src={web5}
+                alt="avatar picture"
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -217,6 +237,7 @@ export default function Home() {
                 height={"100%"}
                 layout="responsive"
                 src={web6}
+                alt="avatar picture"
               />
             </div>
           </div>
